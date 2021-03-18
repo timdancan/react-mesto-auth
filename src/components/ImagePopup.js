129f9b4644c0de17323a75e1789f0bb021b74ddb
timdancan <saqe789@gmail.com>
2021-03-18 13:05:24 +0200

    добавление закрытия по оверлею

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,9 +1,11 @@
 const ImagePopup = ({onClose, card}) => {
-  const onCloseClick = () => {
-    onClose()
+
+  const handleClose = e => {
+    if (e.target.classList.contains("popup")) onClose()
   }
+
   return (
-    <div className={`popup popup_img ${card.isOpen ? 'popup_visiable' : null}`}>
+    <div className={`popup popup_img ${card.isOpen ? 'popup_visiable' : null}`} onClick={handleClose}> 
       <div className="popup__container">
         <div className="popup__img-container">
           <img
@@ -13,7 +15,7 @@ const ImagePopup = ({onClose, card}) => {
           />
         </div>
         <p className="popup__text">{card.element.name}</p>
-        <button className="popup__close popup__close_img page-link" onClick={onCloseClick}></button>
+        <button className="popup__close popup__close_img page-link" onClick={onClose}></button>
       </div>
     </div>
   );
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,7 +1,11 @@
 const PopupWithForm = ({title, name, buttonText, children, isOpen, onClose, handleSubmit}) => {
  
+  const handleClose = e => {
+    if (e.target.classList.contains("popup")) onClose()
+  }
+
   return (
-    <div className={`popup popup_${name} ${isOpen ? 'popup_visiable' : false}`}>
+    <div className={`popup popup_${name} ${isOpen ? 'popup_visiable' : false}`} onClick={handleClose}>
       <div className={`popup__content popup__content_${name}`}>
         <button className={`popup__close popup__close_${name} page-link`} type="button" onClick={onClose}></button>
         <h3 className={`popup__title popup__title_${name}`}>{title}</h3>
